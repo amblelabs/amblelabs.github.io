7d098c560928456d63d35cdf2fa7ea6b1d080532
Theo <git@theo.is-a.dev>
2026-04-02 15:10:52 +0300

    use astro-font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { astroFont } from "astro-font/integration";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroFont()],
+});
