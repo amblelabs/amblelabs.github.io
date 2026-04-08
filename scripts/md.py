f9a7c1c3c535584a3fad185c64ff57e299839379
Theo <git@theo.is-a.dev>
2026-04-08 10:56:05 +0300

    Add support for TikTok in md.py

--- scripts/md.py
+++ scripts/md.py
@@ -4,6 +4,9 @@ def abadge(href: str, name: str) -> str:
     if name == 'youtubeg':
         fullname = 'social/youtube-singular'
         name = 'youtube'
+    elif name == 'tiktokg':
+        fullname = 'social/tiktok-singular'
+        name = 'tiktok'
     elif name == 'kofig':
         fullname = 'donate/kofi-singular'
         name = 'kofi'
